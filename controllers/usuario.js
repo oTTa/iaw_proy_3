351c4e28532bdbbd57d710383d55c84be23748d3
oTTa <lucaotarola@gmail.com>
2017-06-13 23:25:40 -0300

    se corrigieron algunos problemas en el login y en registrarse

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -13,51 +13,45 @@ function registrar(req, res){
 	var params = req.body;
 	//console.log(params);
 	
-	usuario.nombre = trim(params.nombre.toLowerCase());
-	usuario.apellido = trim(params.apellido.toLowerCase());
-	usuario.email = trim(params.email);
-	usuario.role = 'ROLE_USER';
-	usuario.image = null;
+	if (checkData(params,res)){
 
-	checkData(usuario,res);
+		usuario.nombre = trim(params.nombre.toLowerCase());
+		usuario.apellido = trim(params.apellido.toLowerCase());
+		usuario.email = trim(params.email);
+		usuario.role = 'ROLE_USER';
+		usuario.image = null;
 
-	if (params.password){
-		bcrypt.hash(params.password,null,null, function(err,hash){
-			usuario.password = hash;
-			/*usuario.find({email : trim(params.email)}, function (err, docs) {
-        			if (docs.length){
-        			 res.status('409').send({
-							message : 'Ya existe un usuario con ese email.'
-						});
-        			}
-        	});*/
-			usuario.save((err,usuario_store) =>{
-					if (err){
-						res.status('409').send({
-							message : 'Ya existe un usuario con ese email.'
-						});
-					}
-					else{
-						if (usuario_store){
-							res.status('201').send({
-								message : 'Usuario guardado correctamente',
-								usuario : usuario_store
+		if (params.password){
+			bcrypt.hash(params.password,null,null, function(err,hash){
+				usuario.password = hash;
+				usuario.save((err,usuario_store) =>{
+						if (err){
+							res.status('409').send({
+								message : 'Ya existe un usuario con ese email.'
 							});
 						}
 						else{
-							res.status('500').send({
-								message : 'Error al guardar el usuario en la BD.',
-								usuario : usuario_store
-							});
+							if (usuario_store){
+								res.status('201').send({
+									message : 'Usuario guardado correctamente',
+									usuario : usuario_store
+								});
+							}
+							else{
+								res.status('500').send({
+									message : 'Error al guardar el usuario en la BD.',
+									usuario : usuario_store
+								});
+							}
 						}
-					}
-			});				
-		});
-	}
-	else{
-		res.status('400').send({
-			message : 'Debes ingresar una contraseña.'
-		});
+				});				
+			});
+		}
+		else{
+			res.status('400').send({
+				message : 'Debes ingresar una contraseña.'
+			});
+		}
 	}
 }
 
@@ -68,64 +62,72 @@ function checkData(usuario, res){
 		res.status('400').send({
 			message : 'Debes ingresar una nombre.'
 		});
+		return false;
 	}
 	if (usuario.apellido==null || validator.isEmpty(usuario.apellido))
 	{
 		res.status('400').send({
 			message : 'Debes ingresar un apellido.'
 		});
+		return false;
 	}
 	if (usuario.email==null || validator.isEmpty(usuario.email))
 	{
 		res.status('400').send({
 			message : 'Debes ingresar un email.'
 		});
+		return false;
 	}
 	if (!validator.isEmail(usuario.email))
 	{
 		res.status('400').send({
 			message : 'El formato del email es invalido.'
 		});
+		return false;
 	}
+	return true;
 
 }
 
 function login (req, res){
 	var params = req.body;
 
-	check_login(params, res)
+	if (check_login(params, res)){
 
-	var email = trim(params.email);
-	var password = trim(params.password);
+		var email = trim(params.email);
+		var password = trim(params.password);
 
-	Usuario.findOne({email: email}, (err, user) => {
-		if (err){
-			res.status('500').send({
-				message : 'Error en la petición.'
-			});
-		}
-		else{
-			if (!user){
-				res.status('404').send({
-					message : 'No existe el usuario.'
+		Usuario.findOne({email: email}, (err, user) => {
+			if (err){
+				res.status('500').send({
+					message : 'Error en la petición.'
 				});
 			}
 			else{
-				bcrypt.compare(password,user.password, function (err, check){
-					if (check){
-							res.status('200').send({
-								token: jwt.crear_token(user)
+				if (!user){
+					res.status('404').send({
+						message : 'No existe el usuario.'
+					});
+				}
+				else{
+					bcrypt.compare(password,user.password, function (err, check){
+						if (check){
+								res.status('200').send({
+									token: jwt.crear_token(user)
+								});
+						}
+						else{
+							res.status('401').send({
+								message : 'Contraseña invalida.'
 							});
-					}
-					else{
-						res.status('401').send({
-							message : 'Contraseña invalida.'
-						});
-					}
-				});
+						}
+					});
+				}
 			}
-		}
-	});
+		});
+
+	}
+
 }
 
 function check_login (params, res){
@@ -133,12 +135,14 @@ function check_login (params, res){
 		res.status('400').send({
 			message : 'Debes ingresar un email.'
 		});
+		return false;
 	}
 
 	if (params.password==null || validator.isEmpty(params.password)){
 		res.status('400').send({
 			message : 'Debes ingresar una constraseña.'
 		});
+		return false;
 	}
 
 	if (!validator.isEmail(params.email))
@@ -146,7 +150,9 @@ function check_login (params, res){
 		res.status('400').send({
 			message : 'El formato del email es invalido.'
 		});
+		return false;
 	}
+	return true;
 }
 
 module.exports = {
